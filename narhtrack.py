e3733168c09b6cf2153f3d5535d0fb82e02e7230
Tair <109266522+gaolant@users.noreply.github.com>
2022-07-22 21:13:49 +0600

    Update narhtrack.py

--- narhtrack.py
+++ narhtrack.py
@@ -3,3 +3,16 @@ import os
 import time
 from ffpyplayer.player import MediaPlayer
 from cvzone.HandTrackingModule import HandDetector
+
+
+folderPath = "handsigns"
+
+cap = cv2.VideoCapture(0)
+
+imgPath = os.listdir(folderPath)
+
+width, height = 1280, 720
+hs, ws = 120, 213
+signDelay = 15
+signDoing = False
+signCount = 0
